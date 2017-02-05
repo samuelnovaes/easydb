3dce1935b762c1644d7e4b1b2afdad3378fbe436
Samuel Novaes Sena <samuel.novaes.96@gmail.com>
2017-02-05 03:24:13 -0200

    05/02/2017 03:24

--- index.js
+++ index.js
@@ -2,30 +2,41 @@ let fs = require("fs");
 let crypto = require("crypto");
 
 let encrypt = (text, password)=>{
-	password = password || "bN1BX06ULSBKq7IpO3RfJxxa21R9u32F";
-	let cipher = crypto.createCipher("aes-256-ctr", password);
-	let crypted = cipher.update(text, "utf8", "base64");
-	crypted += cipher.final("base64");
-	return crypted;
+	if(password){
+		let cipher = crypto.createCipher("aes-256-ctr", password);
+		let crypted = cipher.update(text, "utf8", "base64");
+		crypted += cipher.final("base64");
+		return crypted;
+	}
 	return text;
 }
 
 let decrypt = (text, password)=>{
-	password = password || "bN1BX06ULSBKq7IpO3RfJxxa21R9u32F";
-	let decipher = crypto.createDecipher("aes-256-ctr", password);
-	let decrypted = decipher.update(text, "base64", "utf8");
-	decrypted += decipher.final("utf8");
-	return decrypted;
+	if(password){
+		let decipher = crypto.createDecipher("aes-256-ctr", password);
+		let decrypted = decipher.update(text, "base64", "utf8");
+		decrypted += decipher.final("utf8");
+		return decrypted;
+	}
 	return text;
 }
 
-module.exports = function(file, password){
-	if(fs.existsSync(file)){
-		this.value = JSON.parse(decrypt(fs.readFileSync(file, "utf-8"), password));
+module.exports = class {
+	constructor(config){
+		this.config = config;
+		try{
+			this.value = JSON.parse(decrypt(fs.readFileSync(config.file, "utf-8"), config.password));
+		}
+		catch(err){
+			if(err.code === "ENOENT"){
+				this.value = config.default;
+			}
+			else{
+				throw err;
+			}
+		}
 	}
-	this.save = ()=>{
-		return new Promise(()=>{
-			fs.writeFileSync(file, encrypt(JSON.stringify(this.value), password), "utf-8");
-		});
+	save(){
+		fs.writeFileSync(this.config.file, encrypt(JSON.stringify(this.value), this.config.password), "utf-8");
 	}
 }
