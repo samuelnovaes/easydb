522939416460c8f95db65c017450bc9d2d901afc
Samuel Novaes <samuel.novaes.96@gmail.com>
2017-08-05 15:10:26 -0300

    05/08/2017 15:10

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # easydb
 
-Easy encrypted JSON databse
+Easy asynchronous encrypted JSON databse
 
 [![NPM](https://nodei.co/npm/easydb.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/easydb/)
 
@@ -14,20 +14,29 @@ $ npm install easydb
 
 ```javascript
 const easydb = require("easydb");
-const db = new easydb({
+
+easydb({
     file: "file.db",
     password: "myPassword",
     default: {location:"Brazil", people:[], num:0} //Initial value
-});
+}).then((db)=>{
 
-//Write or edit data
-db.value.location = "EUA";
-db.value.people.push({name: "Foo", age: 20, gender: "Male"});
-db.value.num++;
+    //Read data
+    console.log(db.value.location);
 
-//Store data
-db.save();
+    //Write or edit data
+    db.value.location = "EUA";
+    db.value.people.push({name: "Foo", age: 20, gender: "Male"});
+    db.value.num++;
 
-console.log(db.value);
-//{location: "EUA", people: [{name: "Foo", age: 20, gender: "Male"}], num: 1}
+    //Store data
+    db.save().then(()=>{
+        console.log("Data Stored!");
+    }).catch((err)=>{
+        console.log('Error on storing data: '+err);
+    });
+
+}).catch((err)=>{
+    console.log('Error on loading database: '+err);
+})
 ```
--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-var fs = require("fs");
-var crypto = require("crypto");
+const fs = require('fs');
+const crypto = require('crypto');
 
-function encrypt(text, password){
-	if(password){
-		var cipher = crypto.createCipher("aes-256-ctr", password);
-		var crypted = cipher.update(text, "utf8", "base64");
-		crypted += cipher.final("base64");
+function encrypt(text, password) {
+	if (password) {
+		let cipher = crypto.createCipher('aes-256-ctr', password);
+		let crypted = cipher.update(text, 'utf8', 'base64');
+		crypted += cipher.final('base64');
 		return crypted;
 	}
 	return text;
 }
 
-function decrypt(text, password){
-	if(password){
-		var decipher = crypto.createDecipher("aes-256-ctr", password);
-		var decrypted = decipher.update(text, "base64", "utf8");
-		decrypted += decipher.final("utf8");
+function decrypt(text, password) {
+	if (password) {
+		let decipher = crypto.createDecipher('aes-256-ctr', password);
+		let decrypted = decipher.update(text, 'base64', 'utf8');
+		decrypted += decipher.final('utf8');
 		return decrypted;
 	}
 	return text;
 }
 
-module.exports = function(config){
-	try{
-		this.value = JSON.parse(decrypt(fs.readFileSync(config.file, "utf-8"), config.password));
-	}
-	catch(err){
-		if(err.code === "ENOENT"){
-			this.value = config.default;
+module.exports = (config) => {
+	return new Promise((resolve, reject) => {
+		let db = {};
+		db.save = () => {
+			return new Promise((resolve, reject) => {
+				fs.writeFile(config.file, encrypt(JSON.stringify(db.value), config.password), (err) => {
+					if (err) reject(err);
+					resolve();
+				});
+			})
 		}
-		else{
-			throw err;
-		}
-	}
-	this.save = function(){
-		fs.writeFileSync(config.file, encrypt(JSON.stringify(this.value), config.password), "utf-8");
-	}
+		fs.readFile(config.file, (err, text) => {
+			if (err) {
+				if (err.code === 'ENOENT') {
+					db.value = config.default;
+				}
+				else {
+					reject(err);
+				}
+			}
+			else db.value = JSON.parse(decrypt(text, config.password));
+			resolve(db);
+		})
+	});
 }
